995b6e0f4bc4c35ef9ee6f364f0758a59811eafe
Perry <perryism@gmail.com>
2020-04-23 11:14:42 -0700

    Include all data

--- setup.py
+++ setup.py
@@ -6,10 +6,15 @@ with open('README.md') as file:
 with open('requirements.txt') as file:
     install_requires = file.readlines()
 
+
 setup(
     name='chernobyl',
     version='0.1',
-    packages=['chernobyl'],
+    packages=['chernobyl', 'chernobyl.new', 'chernobyl.new.controller'],
+    package_data={
+        "chernobyl": ["new/**", "new/controllers/**", "new/controllers/templates/**", "new/templates/**", "new/templates/.*", "new/templates/controllers/**", ],
+    },
+    include_package_data=True,
     install_requires=install_requires,
     url='',
     license='LICENSE.txt',
